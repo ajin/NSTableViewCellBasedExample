cca1f46317398fcdce65cdb969056c0b35be2a66
Ajin <ajin.tuladhar@gmail.com>
2016-09-12 16:58:33 +0200

    Initial Commit

---
+++ NSTableViewCellBasedExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSTableViewCellBasedExample
+//
+//  Created by Ajin Man Tuladhar on 12/09/2016.
+//  Copyright © 2016 Ajin Man Tuladhar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSTableViewCellBasedExample/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  NSTableViewCellBasedExample
+//
+//  Created by Ajin Man Tuladhar on 12/09/2016.
+//  Copyright © 2016 Ajin Man Tuladhar. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
